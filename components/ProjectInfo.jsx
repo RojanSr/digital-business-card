7054f0df09463c7bd3463781c87c3bfe74ad00ed
RojanSr <rairojan1116@gmail.com>
2023-03-04 10:58:31 +0545

    Initial commmit

--- components/ProjectInfo.jsx
+++ components/ProjectInfo.jsx
@@ -4,7 +4,7 @@ export default function ProjectInfo() {
   return (
     <div className="project-info">
       <h1>Made using React</h1>
-      <p>Associated with scrimba LearnReact course</p>
+      <p>Associated with scrimba learnreact course</p>
     </div>
   );
 }
